e1b2945334c33658db483f9f5ad5e9119021f291
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-30 22:08:20 +0530

    final commit

--- main.js
+++ main.js
@@ -2,10 +2,47 @@ var input=document.querySelector("#inputs");
 var button=document.querySelector("#btn");
 var output=document.querySelector("#out");
 
-button.addEventListener("click",clicks);
+button.addEventListener("click",clickHandler);
 
-function clicks(){
+function clickHandler(){
     console.log("clicked");
-  //  var m="clicked";
-  //  output.innerText=m;
+
+    var date=input.value;
+    if(date !== ""){
+      var datearray=date.split("-");
+    console.log(datearray); //yyyy-mm-dd
+    var mmddyyyy=datearray[1]+datearray[2]+datearray[0];
+    console.log(mmddyyyy);
+
+    checkPalindrome(mmddyyyy);
+    }
+    else{
+      console.log("enter date first");
+      showMessage("Enter date first")
+    }    
 }
+
+function checkPalindrome(d){
+  var newd=d;
+  var newarr=newd.split("");
+  var revarr=newarr.reverse();
+  var revd=revarr.join('');
+  console.log(revd)
+
+  if(newd===revd){
+    console.log("pal")
+    showMessage("Wow ! your birthday is a Palindrome!")
+
+  }
+  else{
+    console.log("not pal")
+    showMessage("Your birthday is not a Palindrome")
+  }
+}
+
+
+
+function showMessage(m){
+  output.innerHTML=m;
+}
+
