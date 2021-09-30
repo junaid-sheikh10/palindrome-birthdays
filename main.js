193d8f1b2181f37a4c35740252a4f43750e63f7f
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-30 13:13:52 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var input=document.querySelector("#inputs");
+var button=document.querySelector("#btn");
+var output=document.querySelector("#out");
+
+button.addEventListener("click",clicks);
+
+function clicks(){
+    console.log("clicked");
+  //  var m="clicked";
+  //  output.innerText=m;
+}
